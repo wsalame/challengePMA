0733815aa554c9a82782e8e10845c75f6b3c64bf
wsalame <wissam.salame@polymtl.ca>
2017-06-17 16:37:52 -0400

    empty main

---
+++ src/main/java/me/amp/challenge/main/injector/AppModule.java
@@ -0,0 +1,11 @@
+package me.amp.challenge.main.injector;
+
+import com.google.inject.AbstractModule;
+
+public class AppModule extends AbstractModule {
+	@Override
+	protected void configure() {
+
+		// The other bindings in the app are defined by the @ImplementedBy annotation
+	}
+}
---
+++ src/main/java/me/amp/challenge/main/service/InitServices.java
@@ -0,0 +1,13 @@
+package me.amp.challenge.main.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class InitServices {
+	private final Logger logger = LogManager.getLogger(this.getClass());
+
+	public void startServices() {
+		// TODO Auto-generated method stub
+		
+	}
+}
